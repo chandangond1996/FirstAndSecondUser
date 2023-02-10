e1c17c4cb8716a16a73d8bd907146e304893c8bb
FirstUser <firstuser@gmail.com>
2023-02-10 09:28:09 +0530

    my first commit first user

---
+++ src/gitPractice/Marketing.java
@@ -0,0 +1,10 @@
+package gitPractice;
+
+public class Marketing {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Marketing");
+
+	}
+
+}
---
+++ src/gitPractice/Sales.java
@@ -0,0 +1,10 @@
+package gitPractice;
+
+public class Sales {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Sales");
+
+	}
+
+}
